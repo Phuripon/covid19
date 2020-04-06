1a96d2f2bfa9b1f41d4fc366f0cfa996477ae2e1
Phu <phuripol@hotmail.com>
2020-04-06 18:56:24 +0700

    add model code
    seir model
    run model
    validate model (draft)

---
+++ model_validates.py
@@ -0,0 +1,11 @@
+from method.seir import gen_initial, prepare_input, seir_estimation, seir_df_to_json, summarize_seir, recent_cases_to_doubling_time
+
+# TODO: validate with thailand history
+# load thai_cases
+
+# TODO: validate with other country history
+# TODO: validate with thai province history
+
+# TODO: define measurement (mse sse)
+
+# TODO: define scope of validate (how long? parameter?)
